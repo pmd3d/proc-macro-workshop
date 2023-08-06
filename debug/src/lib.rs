709af35ed45f54e43163a75c977bd3082cf767e5
Jon Lim <jjrr287@gmail.com>
2023-08-06 16:21:00 -0700

    try syn 2.0 instead of 1.0 with this exercise. added field iteration. to finish test, need to recognize field attribute and use the custom format in the field write formatter

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,45 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream};
+use syn::{parse_macro_input, DeriveInput, Data, DataStruct, Fields};
+use quote::quote;
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
 
-    unimplemented!()
+    let fields = match &ast.data {
+        Data::Struct(s) => match &s.fields {
+            Fields::Named(named_fields) => &named_fields.named,
+            _ => panic!("Currently only support named fields"),
+        },
+        _ => panic!("Unsupported data type"),
+    };
+
+    let mut fields_debug = fields.iter().peekable();
+    let mut debug_statements = quote! {};
+
+    while let Some(field) = fields_debug.next() {
+        let field_name = &field.ident;
+        let debug_statement = quote! {
+            write!(f, "{}: \"{}\"", stringify!(#field_name), &self.#field_name)?;
+        };
+
+        debug_statements.extend(debug_statement);
+
+        if fields_debug.peek().is_some() {
+            debug_statements.extend(quote! { write!(f, ", ")?; });
+        }
+    }
+
+    let gen = quote! {
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{} {{ ", stringify!(#name))?;
+                #debug_statements
+                write!(f, " }}")
+            }
+        }
+    };
+
+    gen.into()
 }
